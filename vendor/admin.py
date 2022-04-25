4fa1244f6960b4a0ba57cbb8457c3a078c21475b
Prapanna Bista <bistaprapannabista@gmail.com>
2022-04-25 12:56:36 +0545

    extra signup fields added

---
+++ vendor/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Profile,Vendor
+
+# Register your models here.
+admin.site.register(Vendor)
+admin.site.register(Profile)
---
+++ vendor/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.forms import ModelForm
+from django import forms
+from product.models import Product
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['category', 'image', 'title', 'description', 'price']
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+    
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    phone_number = forms.CharField(max_length=50)
+    date_of_birth = forms.DateField(widget = DateInput)
